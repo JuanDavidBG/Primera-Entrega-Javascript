676b25a67926fd7067fce7e8ef6d4b61b3743081
JuanDavidBG <davidbg581@gmail.com>
2024-12-02 10:35:23 -0500

    quinta subida

--- JS/Ejercicio 2.js
+++ JS/Ejercicio 2.js
@@ -1,7 +1,7 @@
 //  2. escribir un programa que almacene la cadena de caracteres contraseña de una variable, pregunte al usuario por la contraseña e imprima por pantalla si la contraseña introducida por 
 //el usuario coincide con la guardada en la variable sin tener en cuenta mayúsculas y minúsculas, se debe validar que solo se pueda ingresar valores alfanuméricos
 
-const ContraGuardada = "MiContraseña123";
+const ContraGuardada = "Lamalaparalasmonas";
 const ContraIngresada = prompt("Por favor, ingrese su contraseña:");
 
 const esAlfanumerico = /^[a-zA-Z0-9]+$/.test(ContraIngresada);
